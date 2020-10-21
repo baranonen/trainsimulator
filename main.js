dcc352833d02dd3c5a1dd5955e4c76fd8fa536e8
Baran Önen <baranonenn@gmail.com>
2020-10-21 20:00:30 +0300

    Fixes

--- main.js
+++ main.js
@@ -10,14 +10,7 @@ var config = {
         preload: preload,
         create: create,
         update: update
-    },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
-    },
+    }
 };
 
 var game = new Phaser.Game(config);
@@ -61,20 +54,31 @@ document.onkeydown = function(e) {
     }
 }
 
+function findx(place) {
+    x = place * 4071
+    return x
+}
+
 function preload() {
     this.load.image('train', 'assets/train.png');
     this.load.image('taksim', 'assets/taksim.png');
     this.load.image('osmanbey', 'assets/osmanbey.jpg');
     this.load.image('tunnel', 'assets/tunnel.jpg');
+    this.load.image('sisli', 'assets/sisli.jpg');
 }
 
 var taksim
 
 function create() {
     taksim = this.add.image(0, 235, 'taksim').setOrigin(0, 0);
-    tunnel = this.add.image(4071, 235, 'tunnel').setOrigin(0, 0);
-    osmanbey = this.add.image(8143, 235, 'osmanbey').setOrigin(0, 0);
-    tunnel2 = this.add.image(12214, 235, 'tunnel').setOrigin(0, 0);
+    tunnel = this.add.image(findx(1), 235, 'tunnel').setOrigin(0, 0);
+    tunnel2 = this.add.image(findx(2), 235, 'tunnel').setOrigin(0, 0);
+    osmanbey = this.add.image(findx(3), 235, 'osmanbey').setOrigin(0, 0);
+    tunnel3 = this.add.image(findx(4), 235, 'tunnel').setOrigin(0, 0);
+    tunnel4 = this.add.image(findx(5), 235, 'tunnel').setOrigin(0, 0);
+    sisli = this.add.image(findx(6), 235, 'sisli').setOrigin(0, 0);
+    tunnel5 = this.add.image(findx(7), 235, 'tunnel').setOrigin(0, 0);
+    tunnel6 = this.add.image(findx(8), 235, 'tunnel').setOrigin(0, 0);
     taksim.setScale(0.5)
     let train = this.add.image(100, 360, 'train');
     train.setScale(0.2)
@@ -84,6 +88,11 @@ function update() {
     currentDistance += speed
     taksim.x += -1 * speed
     tunnel.x += -1 * speed
-    osmanbey.x += -1 * speed
     tunnel2.x += -1 * speed
+    osmanbey.x += -1 * speed
+    tunnel3.x += -1 * speed
+    tunnel4.x += -1 * speed
+    sisli.x += -1 * speed
+    tunnel5.x += -1 * speed
+    tunnel6.x += -1 * speed
 }
